128910206fe4aedc6a68e029ede594fbb0efdfae
baljeet20 <baljeet20@gmail.com>
2025-02-28 19:03:57 +0530

    ADDED rmq changes

--- src/main/java/org/example/service/EventServiceImpl.java
+++ src/main/java/org/example/service/EventServiceImpl.java
@@ -1,10 +1,11 @@
 package org.example.service;
 
-import jakarta.persistence.EntityManager;
 import org.example.model.mulligan.Event;
 import org.example.repository.mulligan.EventRepository;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,8 +16,18 @@ public class EventServiceImpl implements EventService{
     @Autowired
     EventRepository eventRepository;
 
+    @Autowired
+    AmqpTemplate amqpTemplate;
+
+    @Value("${rabbitmq.exchange}")
+    String exchange;
+
+    @Value("${rabbitmq.routingkey}")
+    private String routingkey;
+
     @Override
     public Event createEvent(Event event) {
+        amqpTemplate.convertAndSend(exchange,routingkey,event.getReplayedMessages().get(0));
         return eventRepository.save(event);
     }
 
